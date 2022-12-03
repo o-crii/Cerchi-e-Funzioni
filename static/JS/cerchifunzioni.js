c0840553992c948ae449afd93609dea2997c34b9
o-crii <cristianpesola@gmail.com>
2022-12-03 10:42:50 +0100

    ultimato il cerchio e l'asse x

--- static/JS/cerchifunzioni.js
+++ static/JS/cerchifunzioni.js
@@ -1,6 +1,10 @@
 let ax = 0.01
 let vx = 1
 let x = 0
+let ay = -0.01
+let vy = 1 
+let h = 400
+let y = h/2
 
 function setup() {
   createCanvas(400, 400);
@@ -10,18 +14,38 @@ function setup() {
 function draw() {
   background(255,255,0)
   circle(x,200,50)
-  circle(x,150,50)
-  circle(x,50,50)
-  circle(x,250,50)
-  circle(x,100,50)
-  circle(x,300,50)
-  circle(x,350,50)
   fill(255,0,0)
 
+
   vx = vx + ax
+  vy = vy + ay
+
   x = x + vx
-  
+  y = y + vy
+
   if(x >= 425){
     x = -25
+  } else if (x <= -25) {
+    x = 425
+  }
+
+  if (y >= 425){
+    y = -25
+  } else if (-25) {
+    y = 425
   }
 }
+
+
+
+let accx= document.querySelector("[name=acceX]")
+let accy= document.querySelector("[name=acceY]")
+
+
+accx.addEventListener("input", function(e){
+  ax = parseFloat(accx.value);
+  })
+
+accy.addEventListener("input", function(e){
+  ay = parseFloat(accy.value);
+  })
